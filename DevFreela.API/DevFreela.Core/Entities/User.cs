75aaefb00cf19184028b6b31bcae73c916bc9977
keveinhitzschky <kevein@alldevelopers.com.br>
2024-08-08 12:06:37 -0300

    Implementação de classe base de entidade e classes iniciais do projeto.

---
+++ DevFreela.API/DevFreela.Core/Entities/User.cs
@@ -0,0 +1,28 @@
+﻿
+namespace DevFreela.Core.Entities
+{
+    public class User : BaseEntity
+    {
+        public User(string fullName, string email, DateTime birthDate)
+        {
+            FullName = fullName;
+            Email = email;
+            BirthDate = birthDate;
+
+            CreatedAt = DateTime.Now;
+            Skills = new List<UserSkill>();
+            Ownedprojects = new List<Project>();
+            FreelanceProjects = new List<Project>();
+            Active = true;
+        }
+
+        public string FullName { get; private set; }
+        public  string Email { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public bool Active { get; set; }
+        public List<UserSkill> Skills { get; private set; }
+        public List<Project> Ownedprojects { get; private set; }
+        public List<Project> FreelanceProjects { get; set; }
+    }
+}
---
+++ DevFreela.API/DevFreela.Core/Entities/UserSkill.cs
@@ -0,0 +1,9 @@
+﻿
+namespace DevFreela.Core.Entities
+{
+    public class UserSkill
+    {
+        public int IdUser { get; private set; }
+        public int IdSkill { get; private set; }
+    }
+}
