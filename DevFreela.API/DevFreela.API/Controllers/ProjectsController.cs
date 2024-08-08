ba9c6ed783da0263d4a1baa99727bdb0a104967c
keveinhitzschky <kevein@alldevelopers.com.br>
2024-08-08 11:30:58 -0300

    Modelo incial do projeto da API

---
+++ DevFreela.API/DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    public class ProjectsController : ControllerBase
+    {
+        [Route("api/Projects")]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
